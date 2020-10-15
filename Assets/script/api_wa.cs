382092d41c41a63175fe61245cf29b94a01cd5e0
afifnuril17 <afif.nuril17@gmail.com>
2020-10-15 10:59:25 +0700

    add file 15-10-2020

--- Assets/script/api_wa.cs
+++ Assets/script/api_wa.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -29,8 +30,9 @@ public class api_wa : MonoBehaviour
 
     void Start()
     {
+        Debug.Log(SystemInfo.operatingSystem);
 
-        if(Tampil1 == false)
+        if (Tampil1 == false)
         {
             StartCoroutine(LoginWa());
         }
@@ -96,7 +98,7 @@ public class api_wa : MonoBehaviour
 
     public void waAwal()
     {
-        Application.OpenURL("https://api.whatsapp.com/send?phone=" + no_waAwal.text.ToString() + "&text=Hai");
+        Application.OpenURL("https://api.whatsapp.com/send?phone=" + no_waAwal.text.ToString() + "&text=Terima%20kasih%20telah%20menghubungi%20kami.%0A%0AMohon%20hapus%20pesan%20ini%20jika%20Anda%20tidak%20mengalami%20kendala%20selama%20proses%20bermain%20games.%0A%0ANamun%20Jika%20Anda%20mengalami%20kendala,%20laporkan%20dengan%20mengisi%20form%20berikut%20Klik:%20https://forms.gle/wxSVV7pvDdg9VxSt5%0A%0ALaporan%20wajib%20kami%20terima%20maksimal%203%20jam%20setelah%20anda%20mengalami%20kendala.%0A%0AKami%20tidak%20menerima%20komunikasi%20via%20whatsapp%20ini%20dan%20juga%20media%20lainnya%20selain%20link%20aduan%20di%20atas.%0A%0ATerima%20kasih");
         //Application.OpenURL("https://api.whatsapp.com/send?phone=6282233477637&text=hai");
         //Debug.Log("https://whatsapp.com/send?phone=" + no_wa.text.ToString() + "&text=hai");
     }
--- Assets/script/api_waakhir.cs
+++ Assets/script/api_waakhir.cs
@@ -60,7 +60,7 @@ public class api_waakhir : MonoBehaviour
 
     public void waAkhir()
     {
-        Application.OpenURL("https://api.whatsapp.com/send?phone=" + no_waAkhir.text.ToString() + "&text=Hai");
+        Application.OpenURL("https://api.whatsapp.com/send?phone=" + no_waAkhir.text.ToString() + "&text=Terima%20kasih%20telah%20menghubungi%20kami.%0A%0AMohon%20hapus%20pesan%20ini%20jika%20Anda%20tidak%20mengalami%20kendala%20selama%20proses%20bermain%20games.%0A%0ANamun%20Jika%20Anda%20mengalami%20kendala,%20laporkan%20dengan%20mengisi%20form%20berikut%20Klik:%20https://forms.gle/wxSVV7pvDdg9VxSt5%0A%0ALaporan%20wajib%20kami%20terima%20maksimal%203%20jam%20setelah%20anda%20mengalami%20kendala.%0A%0AKami%20tidak%20menerima%20komunikasi%20via%20whatsapp%20ini%20dan%20juga%20media%20lainnya%20selain%20link%20aduan%20di%20atas.%0A%0ATerima%20kasih");
         //Application.OpenURL("https://api.whatsapp.com/send?phone=6282233477637&text=hai");
         //Debug.Log("https://whatsapp.com/send?phone=" + no_wa.text.ToString() + "&text=hai");
     }
